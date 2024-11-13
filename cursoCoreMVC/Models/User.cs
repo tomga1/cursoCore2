e6cef532fc77c055777a3e811d9891af9dd934a8
tomga1 <tomasgarcia003@gmail.com>
2024-11-13 10:07:50 -0300

    comit en mvvc  model

---
+++ cursoCoreMVC/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cursoCoreMVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool Admin { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [Url]
+        public string? UrlImagen { get; set; }
+        public DateTime? Fecha_Nacimiento { get; set; }
+    }
+}
