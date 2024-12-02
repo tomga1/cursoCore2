d4b96af7e9e511818a5a23b436d132f7b7a0f8b2
tomga1 <tomasgarcia003@gmail.com>
2024-12-02 17:47:41 -0300

    repositoryCategoria terminado

---
+++ cursoCore2/Repository/CategoriaRepository.cs
@@ -0,0 +1,65 @@
+﻿using cursoCore2API.Data;
+using cursoCore2API.Models;
+using cursoCore2API.Repository.IRepository;
+
+namespace cursoCore2API.Repository
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly StoreContext _context;
+
+        public CategoriaRepository(StoreContext context)
+        {
+            _context = context; 
+        }
+
+
+
+        public bool CrearCategoria(Categoria categoria)
+        {
+            categoria.FechaCreacion = DateTime.Now;
+            _context.categoria.Add(categoria);
+            return Guardar();
+        }
+        public bool ActualizarCategoria(Categoria categoria)
+        {
+            categoria.FechaCreacion = DateTime.Now;
+            _context.categoria.Update(categoria);
+            return Guardar();    
+        }
+
+
+        public bool BorrarCategoria(Categoria categoria)
+        {
+            _context.categoria.Remove(categoria);
+            return Guardar();
+        }
+
+        public bool ExisteCategoria(int id)
+        {
+            return _context.categoria.Any(c => c.Id == id); 
+        }
+
+        public bool ExisteCategoria(string nombre)
+        {
+            bool valor = _context.categoria.Any(c => c.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            return valor;   
+        }
+
+        public Categoria GetCategoria(int categoriaId)
+        {
+            return _context.categoria.FirstOrDefault(c => c.Id == categoriaId);
+        }
+
+
+        public ICollection<Categoria> GetCategorias()
+        {
+            return _context.categoria.OrderBy(c => c.Nombre).ToList();  
+        }
+
+        public bool Guardar()
+        {
+            return _context.SaveChanges() >= 0 ? true : false;  
+        }
+    }
+}
---
+++ cursoCore2/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using cursoCore2API.Models;
+
+namespace cursoCore2API.Repository.IRepository
+{
+    public interface ICategoriaRepository
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int categoriaId);
+        bool ExisteCategoria(int id);
+        bool ExisteCategoria(string nombre);
+
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+
+        bool Guardar();
+    }
+}
