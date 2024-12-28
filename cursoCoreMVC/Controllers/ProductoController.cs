9f197b927ed54dae55b1827ecc0ed315a61dc1c2
tomga1 <tomasgarcia003@gmail.com>
2024-12-28 10:48:16 -0300

    controlador producto y vista Index. ademas inclui Javascript

--- cursoCoreMVC/Controllers/ProductoController.cs
+++ cursoCoreMVC/Controllers/ProductoController.cs
@@ -1,26 +1,38 @@
-﻿using cursoCoreMVC.Services;
+﻿using cursoCoreMVC.Models;
+using cursoCoreMVC.Repository.IRepository;
+using cursoCoreMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cursoCoreMVC.Controllers
 {
     public class ProductoController : Controller
     {
-        private readonly IproductoService _serviceAPI;
+        //private readonly IproductoService _serviceAPI;
 
-        public ProductoController(IproductoService serviceAPI)
+
+        //public ProductoController(IproductoService serviceAPI)
+        //{
+        //    _serviceAPI = serviceAPI;   
+        //}
+
+        //public async Task<IActionResult> Lista()
+        //{
+        //    var productos = await _serviceAPI.Lista();
+        //    return View(productos); 
+        //}
+
+
+        private readonly IProductoRepository _repoProducto;
+
+        public ProductoController(IProductoRepository repoProducto)
         {
-            _serviceAPI = serviceAPI;   
+            _repoProducto = repoProducto;   
         }
 
-        public async Task<IActionResult> Lista()
-        {
-            var productos = await _serviceAPI.Lista();
-            return View(productos); 
-        }
 
         public IActionResult Index()
         {
-            return View();
+            return View(new Productos() { });
         }
     }
 }
